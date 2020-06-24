aac0dbdc647ec22b3eab85b89da2343a2f893020
Linus-Joker <milk790513@yahoo.com.tw>
2020-06-24 20:57:30 +0800

    update crawled

--- crawled/one.py
+++ crawled/one.py
@@ -1,17 +1,25 @@
 import requests as req
 import bs4
 
-def main():
-  url = "https://blog.moneydj.com/news"
-  my_headers = {
-    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
-  }
-  r = req.get(url, headers = my_headers, timeout=5)
-  if r.status_code ==200:
-    soup = bs4.BeautifulSoup(r.text,'html')
-    title = soup.select('.entry-title mh-loop-title > a')
 
-    print(title)
+def main():
+    url = "https://blog.moneydj.com/news"
+
+    my_headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
+    }
+    for i in range(3):
+        payload = {
+            'key': i
+        }
+        r = req.get(url, headers=my_headers, timeout=5, params=payload)
+        if r.status_code == 200:
+            soup = bs4.BeautifulSoup(r.text, 'html.parser')
+            header = soup.find_all("header", class_="mh-loop-header")
+
+            for h in header:
+                title = h.find('h3', 'mh-loop-title').a.string
+                print("標題:", title)
 
 
 main()
