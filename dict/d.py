e1d63f65f7e6a155455e6c48f4ddaa0f6504ec10
Linus-Joker <milk790513@yahoo.com.tw>
2021-01-23 16:38:02 +0800

    update dict

--- dict/d.py
+++ dict/d.py
@@ -3,23 +3,43 @@ car = {
     'model': 'Mustang',
     'year': 1964
 }
-# print(car)
 
-# update() 沒有就新增，有就覆蓋
-# car.update({'color': 'White'})
-# print(car)
 
-# get() 得到指定鍵值，沒有返回None
-# print("model: ", car.get('model'))
+class Car():
 
-# clear() 刪除字典所所有元素
-# print(car)
-# print(car.clear())
+    def print(self, items):
+        print(items)
 
-# dict.values() 返回一個迭代器，
-# 可以使用 list() 来轉換為列表，列表為字典中的所有值
-# print("car dict all value:", list(car.values()))
+    def get(self, items):
+        # get() 得到指定鍵值，沒有返回None
+        print("model: ", items.get('model'))
 
-# pop() 方法删除字典给定键 key 所對應的值，返回值為被删除的值。
-# key值必须给出。 否則，返回default值
-print(car.pop('year'))
+    def update(self, items):
+        # update() 沒有就新增，有就覆蓋
+        items.update({'color': 'White'})
+        print(items)
+
+    def value(self, items):
+        # dict.values() 返回一個迭代器，
+        # 可以使用 list() 来轉換為列表，列表為字典中的所有值
+        print("car dict all value:", list(items.values()))
+
+    def clear(self, items):
+        # clear() 刪除字典所所有元素
+        print(car)
+        print(items.clear())
+
+    def pop(self, items):
+        # pop() 方法删除字典给定键 key 所對應的值，返回值為被删除的值。
+        # key值必须给出。 否則，返回default值
+        print(items.pop('year'))
+
+
+c = Car()
+
+# c.print(car)
+# c.get(car)
+# c.update(car)
+# c.value(car)
+# c.clear(car)
+# c.pop(car)
