8095450f73ea6d4b622900fc1d5d8136e8eca2be
Linus_Joker <milk790513@yahoo.com.tw>
2022-03-30 11:44:55 +0800

    update data stack.

--- data_structure/stack.py
+++ data_structure/stack.py
@@ -1,7 +1,16 @@
+# 堆疊的增加與取出
+
+# 原本的list
 stack = [3, 4, 5]
 stack.append(6)
 stack.append(7)
 print(stack)  # [3, 4, 5, 6, 7]
 
-stack.pop()
-print(stack)  # [3, 4, 5, 6]
+# 反轉list
+# 取出list並重新放入新list
+
+new_stack = []
+for i in range(len(stack)):
+    num = stack.pop()
+    new_stack.append(num)
+print(new_stack)  # [7,6,5,4,3]
