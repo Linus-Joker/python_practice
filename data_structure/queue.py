721917aa0d1183d87110b6929dbbb532953decc3
Linus_Joker <milk790513@yahoo.com.tw>
2022-03-30 11:53:05 +0800

    update data queue.

--- data_structure/queue.py
+++ data_structure/queue.py
@@ -1,7 +1,16 @@
-stack = [3, 4, 5]
-stack.append(6)
-stack.append(7)
-print(stack)
+# 堆疊的增加與取出
 
-stack.pop(0)
-print(stack)
+# 原本的list
+
+queue = [3, 4, 5]
+queue.append(6)
+queue.append(7)
+print(queue)  # [3, 4, 5, 6, 7]
+
+# 取出list並重新放入新list
+new_queue = []
+for i in range(len(queue)):
+    num = queue.pop(0)
+    new_queue.append(num)
+
+print(new_queue)  # [3, 4, 5, 6, 7]
