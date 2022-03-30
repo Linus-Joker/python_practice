5cba68fa2e94f66b25182e7af430e2cf52103ab6
Linus_Joker <milk790513@yahoo.com.tw>
2022-03-30 17:59:00 +0800

    update data stack and queue.

--- data_structure/queue.py
+++ data_structure/queue.py
@@ -1,16 +1,28 @@
-# 堆疊的增加與取出
-
-# 原本的list
-
+# 初始list
 queue = [3, 4, 5]
-queue.append(6)
-queue.append(7)
-print(queue)  # [3, 4, 5, 6, 7]
+
 
 # 取出list並重新放入新list
-new_queue = []
-for i in range(len(queue)):
-    num = queue.pop(0)
-    new_queue.append(num)
+def get_queue(queue):
+    new_queue = []
+    for i in range(len(queue)):
+        num = queue.pop(0)
+        new_queue.append(num)
 
-print(new_queue)  # [3, 4, 5, 6, 7]
+    # print(len(new_queue))
+    return new_queue
+
+
+def main():
+    queue.append(6)
+    queue.append(7)
+    print("佇列共有%d筆數值:" % len(queue))
+    print(queue)  # [3, 4, 5, 6, 7]
+
+    new_queue = get_queue(queue)
+    print("重新放入佇列後共有%d筆數值:" % len(new_queue))
+    print(new_queue)  # [3, 4, 5, 6, 7]
+
+
+if __name__ == '__main__':
+    main()
--- data_structure/stack.py
+++ data_structure/stack.py
@@ -1,16 +1,29 @@
-# 堆疊的增加與取出
-
-# 原本的list
+# 初始list
 stack = [3, 4, 5]
-stack.append(6)
-stack.append(7)
-print(stack)  # [3, 4, 5, 6, 7]
+
 
 # 反轉list
 # 取出list並重新放入新list
+def reverse_stack(stack):
+    new_stack = []
+    for i in range(len(stack)):
+        num = stack.pop()
+        new_stack.append(num)
 
-new_stack = []
-for i in range(len(stack)):
-    num = stack.pop()
-    new_stack.append(num)
-print(new_stack)  # [7,6,5,4,3]
+    return new_stack
+
+
+def main():
+    # 堆疊的增加
+    stack.append(6)
+    stack.append(7)
+    print("堆疊共有%d筆數值:" % len(stack))
+    print(stack)  # [3, 4, 5, 6, 7]
+
+    new_stack = reverse_stack(stack)
+    print("堆疊反轉後共有%d筆數值:" % len(new_stack))
+    print(new_stack)  # [7,6,5,4,3]
+
+
+if __name__ == '__main__':
+    main()
